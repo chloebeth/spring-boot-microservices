11c1783b265b947c24e447f4e39046c651972748
chloebeth <chloe.e.muncy@gmail.com>
2020-03-23 02:31:47 -0400

    Code clean up

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResources.java
@@ -23,12 +23,12 @@ public class MovieCatalogResources {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		// Get all rated Movie IDs
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/foo" + userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 		
 		// Return all movies
 		return ratings.getUserRating().stream().map(rating -> {
 			// For each movie ID, call movie info service and get details
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getName(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getName(), Movie.class);
 			return new CatalogItem(movie.getName(), "Desc",  rating.getRating());
 		})
 		.collect(Collectors.toList());
